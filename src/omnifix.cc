01049bc21936deb4351177675820b835273a3bc5
aixxe <me@aixxe.net>
2025-07-23 12:52:08 +0100

    Add music select thumbnail patch for IIDX 32

--- src/omnifix.cc
+++ src/omnifix.cc
@@ -169,6 +169,18 @@ auto patch_video_music_list_xml_path(auto ptr)
 }
 
 /**
+ * Load thumbnails in music select for entries below ID 10000.
+ */
+auto patch_thumbnail_file_path(auto&& bm2dx)
+{
+    auto constexpr prefix = "/data/graphic/thumbnail/";
+    auto constexpr bytes = std::to_array("%d_thum.png");
+
+    if (auto ptr = memory::find(bm2dx, memory::to_pattern(prefix), true))
+        add_patch(ptr + std::strlen(prefix), bytes);
+}
+
+/**
  * Find and alter paths to point to custom files.
  */
 auto setup_omnimix_path_patch(auto&& bm2dx)
@@ -193,6 +205,9 @@ auto setup_omnimix_path_patch(auto&& bm2dx)
 
     patch_video_music_list_xml_path(memory::find(bm2dx,
         prefix + " ? " + memory::to_pattern("/video_music_")));
+
+    // More optional stuff for IIDX 32.
+    patch_thumbnail_file_path(bm2dx);
 }
 
 /**
