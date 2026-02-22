fd87a7e75ae3fab78d973aa275a6b34c263c8f7d
aixxe <me@aixxe.net>
2026-02-22 00:12:53 +0000

    Slightly clean up game structures

--- src/bm2dx.h
+++ src/bm2dx.h
@@ -6,11 +6,11 @@ namespace bm2dx
     {
         /* 0x0000 */ std::uint8_t magic[4];
         /* 0x0004 */ std::int32_t version;
-    };
+    }; static_assert(sizeof(mdb_common) == 0x8);
 
-    struct mdb_v27
+    struct mdb_v27: mdb_common
     {
-	    using index_type = std::int16_t;
+        using index_type = std::int16_t;
 
         auto static constexpr level_offset = 0x120;
         auto static constexpr index_offset = 0x3B0;
@@ -18,16 +18,14 @@ namespace bm2dx
         auto static constexpr is_supported(auto version) -> bool
             { return version >= 27 && version <= 31; }
 
-        /* 0x0000 */ std::uint8_t magic[4];
-        /* 0x0004 */ std::int32_t version;
         /* 0x0008 */ std::int16_t entries;
         /* 0x000A */ std::int16_t max_entries;
         /* 0x000C */ std::uint8_t pad_000C[4];
-    };
+    }; static_assert(sizeof(mdb_v27) == 0x10);
 
-    struct mdb_v32
+    struct mdb_v32: mdb_common
     {
-	    using index_type = std::int32_t;
+        using index_type = std::int32_t;
 
         auto static constexpr level_offset = 0x3EC;
         auto static constexpr index_offset = 0x67C;
@@ -35,9 +33,7 @@ namespace bm2dx
         auto static constexpr is_supported(auto version) -> bool
             { return version >= 32 && version <= 33; }
 
-        /* 0x0000 */ std::uint8_t magic[4];
-        /* 0x0004 */ std::int32_t version;
         /* 0x0008 */ std::int32_t entries;
         /* 0x000C */ std::int32_t max_entries;
-    };
+    }; static_assert(sizeof(mdb_v32) == 0x10);
 }
